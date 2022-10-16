669ad0dbf9b8439c1720cab8c22027610b5ad523
Kunhao ZHENG <dyekuu@gmail.com>
2022-10-16 23:58:31 +0800

    [Metamath Fix] numbertheory_notequiv2i2jasqbsqdiv8 (#49)

--- metamath/test/numbertheory-notequiv2i2jasqbsqdiv8.mm
+++ metamath/test/numbertheory-notequiv2i2jasqbsqdiv8.mm
@@ -1,7 +1,7 @@
 $(
   @{
     @( (Contributed by Kunhao Zheng, 4-May-2021.) @)
-    numbertheory-notequiv2i2jasqbsqdiv8 @p |- -. ( ( E. i e. ZZ A = ( 2 x. i ) /\ E. j e. ZZ B = ( 2 x. j ) ) <-> E. k e. ZZ ( ( A ^ 2 ) + ( B ^ 2 ) ) = ( 8 x. k ) ) @=
+    numbertheory-notequiv2i2jasqbsqdiv8 @p |- -. ( A. a e. ZZ A. b e. ZZ ( E. i e. ZZ a = ( 2 x. i ) /\ E. j e. ZZ b = ( 2 x. j ) ) <-> E. k e. ZZ ( ( a ^ 2 ) + ( b ^ 2 ) ) = ( 8 x. k ) ) @=
       ? @.
   @}
 $)
