30e293ac8bb14d218a68f4ce75fd5a7460fe0f4d
Kunhao ZHENG <dyekuu@gmail.com>
2022-10-16 21:57:23 +0800

    [Lean Metamath HOL Light Fix] Amc12a 2021 p12 (#43)
    * fix
    
    * fix

--- lean/src/test.lean
+++ lean/src/test.lean
@@ -2282,7 +2282,7 @@ theorem amc12a_2021_p12
   (f : ℂ → ℂ)
   (h₀ : ∀ z, f z = z^6 - 10 * z^5 + a * z^4 + b * z^3 + c * z^2 + d * z + 16)
   (h₁ : ∀ z, f z = 0 → (z.im = 0 ∧ 0 < z.re ∧ ↑(int.floor z.re) = z.re)) :
-  b = 88 :=
+  b = -88 :=
 begin
   sorry
 end
