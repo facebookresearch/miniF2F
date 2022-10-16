813260e0e6286c0c4af74d369d9d0a919b401089
Kunhao ZHENG <dyekuu@gmail.com>
2022-10-16 22:02:34 +0800

    [Lean Metamath Fix] imo_1966_p5 (#44)

--- lean/src/valid.lean
+++ lean/src/valid.lean
@@ -1979,10 +1979,13 @@ theorem imo_1966_p5
   (h₃ : a 2 ≠ a 3)
   (h₄ : a 2 ≠ a 4)
   (h₅ : a 3 ≠ a 4)
-  (h₆ : abs (a 1 - a 2) * x 2 + abs (a 1 - a 3) * x 3 + abs (a 1 - a 4) * x 4 = 1)
-  (h₇ : abs (a 2 - a 1) * x 1 + abs (a 2 - a 3) * x 3 + abs (a 2 - a 4) * x 4 = 1)
-  (h₈ : abs (a 3 - a 1) * x 1 + abs (a 3 - a 2) * x 2 + abs (a 3 - a 4) * x 4 = 1)
-  (h₉ : abs (a 4 - a 1) * x 1 + abs (a 4 - a 2) * x 2 + abs (a 4 - a 3) * x 3 = 1) :
+  (h₆ : a 1 > a 2)
+  (h₇ : a 2 > a 3)
+  (h₈ : a 3 > a 4)
+  (h₉ : abs (a 1 - a 2) * x 2 + abs (a 1 - a 3) * x 3 + abs (a 1 - a 4) * x 4 = 1)
+  (h₁₀ : abs (a 2 - a 1) * x 1 + abs (a 2 - a 3) * x 3 + abs (a 2 - a 4) * x 4 = 1)
+  (h₁₁ : abs (a 3 - a 1) * x 1 + abs (a 3 - a 2) * x 2 + abs (a 3 - a 4) * x 4 = 1)
+  (h₁₂ : abs (a 4 - a 1) * x 1 + abs (a 4 - a 2) * x 2 + abs (a 4 - a 3) * x 3 = 1) :
   x 2 = 0 ∧ x 3 = 0 ∧ x 1 = 1 / abs (a 1 - a 4) ∧ x 4 = 1 / abs (a 1 - a 4) :=
 begin
   sorry
