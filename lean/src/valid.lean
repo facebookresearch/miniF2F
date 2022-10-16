5ff269cff3021d2f24c20d28abf3236f70509343
Kunhao ZHENG <dyekuu@gmail.com>
2022-10-16 20:52:39 +0800

    [Lean Metamath Fix] imo_1974_p5 (#37)

--- lean/src/valid.lean
+++ lean/src/valid.lean
@@ -1599,7 +1599,8 @@ end
 
 theorem imo_1974_p5
   (a b c d s : ℝ)
-  (h₀ : s = a / (a + b + d) + b / (a + b + c) + c / (b + c + d) + d / (a + c + d)) :
+  (h₀ : 0 < a ∧ 0 < b ∧ 0 < c ∧ 0 < d)
+  (h₁ : s = a / (a + b + d) + b / (a + b + c) + c / (b + c + d) + d / (a + c + d)) :
   1 < s ∧ s < 2 :=
 begin
   sorry
