3e577e60072f3e50fdba1bca0a45c9a130164488
Kunhao ZHENG <dyekuu@gmail.com>
2022-10-17 00:08:51 +0800

    [Lean Metamath HOL Light Fix] (#50)
    * fix
    
    * rename
    
    * fix

--- lean/src/test.lean
+++ lean/src/test.lean
@@ -228,7 +228,7 @@ begin
   sorry
 end
 
-theorem imo_2007_p6
+theorem imosl_2007_algebra_p6
   (a : ℕ → nnreal)
   (h₀ : ∑ x in finset.range 100, ((a (x + 1))^2) = 1) :
   ∑ x in finset.range 99, ((a (x + 1))^2 * a (x + 2)) + (a 100)^2 * a 1 < 12 / 25 :=
